f33a10b6228cb1de74a26c7e9072ec12b3d2d3fc
Aman <21bee070@nith.ac.in>
2023-12-28 03:01:33 +0530

    Ui update

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -14,12 +14,8 @@ export default function ForgotPassword() {
     <>
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-            alt="Your Company"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+      
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight dark:text-white text-slate-600">
             Forgot Password
           </h2>
         </div>
@@ -27,7 +23,7 @@ export default function ForgotPassword() {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <div className="space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+              <label htmlFor="email" className="block text-sm font-medium leading-6 dark:text-white text-slate-600">
                 Email address
               </label>
               <div className="mt-2">
@@ -38,7 +34,7 @@ export default function ForgotPassword() {
                   autoComplete="email"
                   onChange={(e) => setEmail(e.target.value)}
                   required
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 dark:bg-black/5 bg-black/25 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
